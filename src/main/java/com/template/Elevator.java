b883e5f9bc164124c5943cfa15651b8617fd13a1
Cande De Franco <candedefranco@Candes-MBP.fibertel.com.ar>
2024-09-06 17:36:49 -0300

    hice el tp4 de java

--- src/main/java/com/template/Elevator.java
+++ src/main/java/com/template/Elevator.java
@@ -1,9 +1,52 @@
 package com.template;
 
 public class Elevator {
+    int minFloor;
+    int maxFloor;
+    int maxPeople;
+    int currentFloor;
+    int passengerCount;
 
+    //constructor
+    public Elevator(int minFloor, int maxFloor, int maxPeople) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+        this.maxPeople = maxPeople;
+        this.currentFloor = 0;
+        this.passengerCount = 0;
+    }
 
+    //getters
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
 
+    public int getPassengerCount() {
+        return passengerCount;
+    }
+
+    //setters
+    public void moveToFloor(int newFloor) {
+        if (minFloor <= newFloor & newFloor <= maxFloor) {
+            currentFloor = newFloor;
+        }
+    }
+
+    public void addPassenger() {
+        if (passengerCount + 1 <= maxPeople) {
+            passengerCount ++;
+        }
+    }
+
+    public void removePassenger() {
+        if (passengerCount - 1 >= 0) {
+            passengerCount --;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
     // Implement this class by observing the tests in this repo. From those you will be able
     // to pick what the constructors params are, what the methods are and what they should do.
 
